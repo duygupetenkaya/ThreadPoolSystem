18047534cc2639c257be8476571fece04c4744ab
DuyguP <elifduygupetenkaya@gmail.com>
2021-06-06 19:54:23 +0300

    Thread and ThreadPool behaviours are added...

--- ThreadPoolSystemSource/src/Main.java
+++ ThreadPoolSystemSource/src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
-    public static void main(String [] args){
-    ThreadPool threadPool=ThreadPool.ThreadPoolCreate();
+    public static void main(String [] args) throws InterruptedException {
+        Runnable r = () -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " is executing task.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
 
+        ThreadPool threadPool=ThreadPool.ThreadPoolCreate();
+
+        threadPool.execute(r);
+        threadPool.shutdown();
     }
 }
