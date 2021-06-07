0228458638cbff31f2fcc788a534facfc05819c3
DuyguP <elifduygupetenkaya@gmail.com>
2021-06-07 08:18:22 +0300

    Updates

--- ThreadPoolSystemSource/src/Watcher.java
+++ ThreadPoolSystemSource/src/Watcher.java
@@ -1,8 +1,7 @@
 //Abstract Observer
 public abstract class Watcher {
-    abstract void Update(HThread thread);
+    abstract void Update(Thread thread);
 
-    abstract void Update(LThread thread);
 }
 
 //Concrete Observer
@@ -12,19 +11,11 @@ class StateWatcher extends Watcher {
     private LThread Lthread;
 
     @Override
-    void Update(HThread thread) {
-        Hthread = thread;
-        threadState = thread.getThreadState();
+    void Update(Thread thread) {
+             threadState = thread.getThreadState();
 
        /* System.out.println("Notified " + watcher_name + " of " + securityCamName +
                 "'s " + "change to " + threadState);*/
     }
 
-    @Override
-    void Update(LThread thread) {
-        Lthread = thread;
-        threadState = thread.getThreadState();
- /* System.out.println("Notified " + watcher_name + " of " + securityCamName +
-                "'s " + "change to " + threadState);*/
-    }
 }
